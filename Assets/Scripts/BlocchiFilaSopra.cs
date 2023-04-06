0eb3230de3665bebf324f7a88588815fd027840b
RiccardoAlbarello <albarello.riccardo@gmail.com>
2023-04-07 01:35:37 +0200

    Alcune bestemmie Joystick

--- Assets/Scripts/BlocchiFilaSopra.cs
+++ Assets/Scripts/BlocchiFilaSopra.cs
@@ -2,18 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlocchiCentrali : MonoBehaviour
+public class BlocchiFilaSopra : MonoBehaviour
 {
+
     public int sizeX;
     public int sizeZ;
 
+    public int distanceZ;
+
     public GameObject muro;
-
-    public float distanzaX;
-    public float distanzaZ;
-
-    public int sizeXLato;
-    public int sizeZLato;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +20,8 @@ public class BlocchiCentrali : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
 
     void Matrix()
     {
@@ -35,11 +31,8 @@ public class BlocchiCentrali : MonoBehaviour
             {
                 GameObject tmp = Instantiate(muro);
                 tmp.transform.SetParent(this.transform);
-                tmp.transform.localPosition = new Vector3(i * distanzaX, 0, j * distanzaZ);
+                tmp.transform.localPosition = new Vector3(i, 0, j * distanceZ);
             }
         }
-
-       
-    
     }
 }
--- Assets/Scripts/PlayerMovement2.cs
+++ Assets/Scripts/PlayerMovement2.cs
@@ -1,25 +1,86 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement2 : MonoBehaviour
 {
-    Vector3 PlayerMovementInput;
+    public enum GamepadButton
+    {
+        
+    }
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.W))
+    //    {
+    //        transform.(0, 0, 2f);
+    //    }
 
-    public Rigidbody playerBody;
+    //    if (Input.GetKeyDown(KeyCode.S))
+    //    {
+    //        transform.Translate(0, 0, -2f);
+    //    }
 
-    public float speed;
+    //    if (Input.GetKeyDown(KeyCode.D))
+    //    {
+    //        transform.Translate(2f, 0, 0);
+    //    }
+
+    //    if (Input.GetKeyDown(KeyCode.A))
+    //    {
+    //        transform.Translate(-2f, 0, 0);
+    //    }
+    //}
+
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    public float timeToMove = 0.5f;
 
     private void Update()
     {
-        PlayerMovementInput = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        if (Input.GetKeyDown(KeyCode.W) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.forward * 2));
+        }
 
-        MovePlayer();
+        if (Input.GetKeyDown(KeyCode.A) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.left * 2));
+        }
+
+        if (Input.GetKeyDown(KeyCode.S) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.back * 2));
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && !isMoving)
+        {
+            StartCoroutine(MovePlayer(Vector3.right * 2));
+        }
     }
 
-    void MovePlayer()
+    private IEnumerator MovePlayer(Vector3 direction)
     {
-        Vector3 MoveVector = transform.TransformDirection(PlayerMovementInput) * speed;
-        playerBody.velocity = new Vector3(MoveVector.x, 0f, MoveVector.z);
+        isMoving = true;
+
+        float elapsedTime = 0;
+
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+
+
+        isMoving = false;
     }
+
 }
